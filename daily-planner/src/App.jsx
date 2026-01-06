bf902a2e0d41e1df46f8de52ec6699a8068bd71f
Dellya <dellyadellya061@gmail.com>
2026-01-06 11:30:37 +0700

    add initial task state and TaskInput component

--- daily-planner/src/App.jsx
+++ daily-planner/src/App.jsx
@@ -1,15 +1,20 @@
-import './App.css'
+import { useState } from 'react'
 import Header from './component/Header'
 import TaskInput from './component/TaskInput'
 import TaskItem from './component/TaskItem'
 import TaskList from './component/TaskList'
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTask = () => {
+
+  }
 
   return (
     <>
       <Header />
-      <TaskInput />  
+      <TaskInput onAddTask = {handleAddTask} />  
       <TaskItem />
       <TaskList />
     </>
--- daily-planner/src/component/TaskInput.jsx
+++ daily-planner/src/component/TaskInput.jsx
@@ -1,7 +1,9 @@
-function TaskInput() {
+function TaskInput({ onAddTask }) {
+  
   return (
     <div>
-     
+      <input type="text" placeholder="Typing Your Task" />
+      <button onClick={onAddTask}>Add</button>
     </div>
   );
 }
