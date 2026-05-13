23900acd538a2f8bcb0f5f6179202249b5a499ba
Dellya <dellyadellya061@gmail.com>
2026-05-13 12:05:56 +0700

    Fix css for 'not tasks yet'

--- daily-planner/src/component/TaskList.jsx
+++ daily-planner/src/component/TaskList.jsx
@@ -2,7 +2,7 @@ import TaskItem from './TaskItem';
 
 function TaskList({ tasks, onToggleTask, onDeleteTask }) {
    if (tasks.length === 0) {
-    return <p>No tasks yet</p>
+    return <p className='no-tasks-yet'>Not tasks yet</p>
   }
   return (
     <>
